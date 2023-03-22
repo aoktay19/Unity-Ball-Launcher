bab6067ec63beca635a5d10557ff3a5cd13b4e12
ali.oktay <ali.oktay@trendyol.com>
2023-03-22 12:41:37 +0300

    moving the ball with touching

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -3,24 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D currentBallRigidBody;
+    private Camera mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(!Touchscreen.current.primaryTouch.press.isPressed){
+            currentBallRigidBody.isKinematic = false;
             return;
         }
 
-        Vector2 touchScreen = Touchscreen.current.primaryTouch.position.ReadValue();
+        currentBallRigidBody.isKinematic = true;
 
-        Debug.Log(touchScreen);
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+        
+        currentBallRigidBody.position = worldPosition;
     }
 }
