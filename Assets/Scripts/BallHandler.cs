51b45a51c6d69fa3208176dd03d092b720a5134e
ali.oktay <ali.oktay@trendyol.com>
2023-03-23 13:10:21 +0300

    Launching Ball.

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,9 +5,13 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D currentBallRigidBody;
-    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Rigidbody2D pivot;
     [SerializeField] private float detachDelay;
+    [SerializeField] private float respawnDelay;
+
+    private Rigidbody2D currentBallRigidBody;
+    private SpringJoint2D currentBallSpringJoint;
     private Camera mainCamera;
     private bool isDragging;
 
@@ -15,6 +19,7 @@ public class BallHandler : MonoBehaviour
     void Start()
     {
         mainCamera = Camera.main;
+        SpawnNewBall();
     }
 
     // Update is called once per frame
@@ -40,6 +45,15 @@ public class BallHandler : MonoBehaviour
         
         currentBallRigidBody.position = worldPosition;
     }
+    private void SpawnNewBall(){
+        GameObject ballInstance =Instantiate(ballPrefab,pivot.position, Quaternion.identity);
+
+        currentBallRigidBody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+
+        currentBallSpringJoint.connectedBody = pivot;
+    }
+
     private void LaunchBall(){
         currentBallRigidBody.isKinematic = false;
         currentBallRigidBody = null;
@@ -50,5 +64,7 @@ public class BallHandler : MonoBehaviour
     private void DetachBall(){
         currentBallSpringJoint.enabled = false;
         currentBallSpringJoint = null;
+
+        Invoke(nameof(SpawnNewBall), respawnDelay);
     }
 }
